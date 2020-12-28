06542eff0edc1d668c43de860d429ba8bc7d5ebb
Joaquin Tamargo <wakitamargo@gmail.com>
2020-12-28 14:06:42 +0100

    R script tests

---
+++ prueba.R
@@ -0,0 +1,11 @@
+print("hello world")
+
+5+5
+
+## Esto no lo tiene que leer
+
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("ChIPseeker")
+library(ChIPseeker)
