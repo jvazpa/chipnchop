f73d226b340f47418dab5cd36690821bdbad3bd3
Joaquín Tamargo Azpilicueta <ParaisoNoFiscal@MBP-de-Joaquin.home>
2021-01-12 21:18:03 +0100

    We're getting closer to parallelization. Error-prone script

--- sample_processing.sh
+++ sample_processing.sh
@@ -9,9 +9,10 @@
 ## READING PARAMS
 
 SAMPLE=$1
-j=$2
+i=$2
 NUMOFSAMP=$3
-INSDIR=$4
+TOTNUM=$4
+INSDIR=$5
 
 ## QUALITY CONTROL
 
@@ -19,44 +20,46 @@ fastqc $SAMPLE
 
 ## MAPPING READS
 
-bowtie2 -x ../../genome/index -U $SAMPLE -S chip.sam
+bowtie2 -x ../../genome/index -U $SAMPLE_$i -S sample_$i.sam
+
+samtools sort -o sample_$i.bam sample_$i.sam
+rm sample_$i.sam
+samtools index sample_$i.bam
+bamCoverage -bs 10 --normalizeUsing CPM --bam sample_$i.bam -o sample_$i.bw --verbose True
+
+
+## TRANSCRIPT ASSEMBLY
+
+stringtie -G ../../annotation/annotation.gtf -o sample_$i.gtf -l sample_$i sample_$i.bam
+
+
+## Preparing merge list file for transcriptome merging
+
+echo ${SAMPLE_DIR}/sample_$i.gtf >> ../../results/merge_list.txt
+
 
 ## BLACKBOARD
 
-
-##############################################################
-
+echo "sample_$i done!" >> ../../results/blackboard
 
 
+echo ""
+echo "Sample $i processing is done"
+echo ""
+
+## Reading the blackboard (counting all lines)
+
+NUM_PROC=$(wc -l ../../results/blackboard | awk '{ print $1 }')
 
 
-
-## Sample quality control and read mapping to reference genome
-if [ -f <accession_sra>_2.fastq ]
+if [ $NUM_PROC -eq $TOTNUM ]
 then
-   fastqc <accession_sra>_1.fastq
-   fastqc <accession_sra>_2.fastq
-
-   hisat2 --dta -x ../../genome/index -1 <accession_sra>_1.fastq -2 <accession_sra>_2.fastq -S sample<N>.sam
-else
-   fastqc <accession_sra>_1.fastq
-
-   hisat2 --dta -x ../../genome/index -U <accession_sra>_1.fastq -S sample<N>.sam
+	echo "All sample processing (both CHIP and INPUT) have succesfully finished. Transcriptome merging is running."
+	echo ""
+	qsub -o merge -N merge $INSDIR/chipnchop/transcriptome_merging.sh ${SAMPLE}/../../results $INSDIR 
 fi
 
-## Generting sorted bam file
-samtools sort -o sample<N>.bam sample<N>.sam
-rm sample<N>.sam
-rm *.fastq
-samtools index sample<N>.bam
-bamCoverage -bs 10 --normalizeUsing CPM --bam sample<N>.bam -o sample<N>.bw
 
 
-## Transcript assembly
-stringtie -G ../../annotation/annotation.gtf -o sample<N>.gtf -l sample<N> sample<N>.bam
 
-## Preparing merge list file for transcriptome merging
-echo /home/<grupo>/<exp>/samples/sample<N>/sample<N>.gtf >> ../../results/merge_list.txt
 
-## Gene Expression Quantification
-stringtie -e -B -G ../../annotation/annotation.gtf -o sample<N>.gtf sample<N>.bam
