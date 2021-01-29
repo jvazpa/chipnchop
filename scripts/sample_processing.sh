07a727b1167fd2ad5f09c21d3e7078cef2de4446
Joaquin Tamargo <wakitamargo@gmail.com>
2021-01-30 00:13:39 +0100

    Fixed a bug that wouldn't let -HI mode to run. Deleted some features that were included as a part of a RNAseq pipeline.

--- scripts/sample_processing.sh
+++ scripts/sample_processing.sh
@@ -15,6 +15,8 @@ SAMPLE=$1
 i=$2
 TOTNUM=$3
 INSDIR=$4
+RESDIR=$5
+SAMDIR=$6
 
 ## QUALITY CONTROL
 fastqc $SAMPLE
@@ -24,7 +26,6 @@ bowtie2 -x ../../genome/index -U $SAMPLE -S sample_$i.sam
 
 samtools sort -o sample_$i.bam sample_$i.sam
 rm sample_$i.sam
-rm $SAMPLE
 samtools index sample_$i.bam
 
 
@@ -33,7 +34,7 @@ stringtie -G ../../annotation/annotation.gtf -o sample_$i.gtf -l sample_$i sampl
 
 
 ## Preparing merge list file for transcriptome merging
-echo ${SAMPLE_DIR}/sample_$i.gtf >> ../../results/merge_list.txt
+echo ${SAMDIR}/sample_$i.gtf >> ../../results/merge_list.txt
 
 
 ## BLACKBOARD -> This would be useful for parallelization
@@ -43,17 +44,3 @@ echo "sample_$i done!" >> ../../results/blackboard
 echo ""
 echo "Sample $i processing is done"
 echo ""
-
-## Reading the blackboard (counting all lines)
-NUM_PROC=$(wc -l ../../results/blackboard | awk '{ print $1 }')
-
-
-if [ $NUM_PROC -eq $TOTNUM ]
-then
-	echo "All sample processing (both CHIP and INPUT) have succesfully finished. Transcriptome merging is running."
-	echo ""
-	
-	bash $INSDIR/scripts/transcriptome_merging.sh ${SAMPLE}/../../results
-	
-	## (error-prone): qsub -o merge -N merge $INSDIR/chipnchop/transcriptome_merging.sh ${SAMPLE}/../../results $INSDIR 
-fi
